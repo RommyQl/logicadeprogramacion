5fc3e455125cdf0b6f6c1497cad7e2b2daadefa0
RommyQl <rommyquispe123@gmail.com>
2026-06-23 21:10:16 -0500

    Add files via upload

---
+++ practica.py
@@ -0,0 +1,18 @@
+
+nombre=input("ingrese su nombre")
+programa= input("ingrese nombre del programa de estudios")
+promedio=float(input("ingrese su promedio del (0-20) : "))
+
+if promedio >= 17:
+    print("beca de exelencia ")
+    print("su matricula es de: S/0.00 ")
+    
+elif promedio>=13 and promedio<=16.9:
+    print("admitido regular")
+    print("matricula a pagar: S/150.00" )
+    print (f"nombre{nombre}")  
+elif promedio<=13:
+    print("lista de espera")
+    print (f"nombre{nombre}")  
+
+ 
